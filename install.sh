fd219e08eedd90d0a411148a1b9b7cd7e4009213
Jerel Miller <jerelmiller@gmail.com>
2017-09-09 23:45:20 -0700

    Add asdf installation to install

--- install.sh
+++ install.sh
@@ -117,6 +117,12 @@ if ! command -v nvm > /dev/null; then
   nvm install node
 fi
 
+if ! command -v asdf > /dev/null; then
+  echo "Installing asdf"
+
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
+fi
+
 if [ ! -d $HOME/code/fonts ]; then
   cd $HOME/code
   git clone git@github.com:powerline/fonts.git
