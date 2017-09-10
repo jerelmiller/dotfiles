2769481c4487dc5665bf3c7d73f9fb4c481815bc
Jerel Miller <jerelmiller@gmail.com>
2017-09-09 23:59:01 -0700

    Move source of bash_profile to very bottom

--- install.sh
+++ install.sh
@@ -117,8 +117,6 @@ gem_install_or_update "bundler"
 number_of_cores=$(sysctl -n hw.ncpu)
 bundle config --global jobs $((number_of_cores - 1))
 
-source ~/.bash_profile
-
 if ! command -v nvm > /dev/null; then
   echo "Installing nvm..."
 
@@ -147,3 +145,5 @@ if [ ! -d $HOME/code/fonts ]; then
   ./install.sh
   cd $currentdir
 fi
+
+source $HOME/.bash_profile
