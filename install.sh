404a465f6b6d511f90aea4112e971bd0dc996ba0
Jerel Miller <jerelmiller@gmail.com>
2018-03-21 00:27:22 -0700

    Remove commands to setup vim

--- install.sh
+++ install.sh
@@ -10,29 +10,9 @@ gem_install_or_update() {
   fi
 }
 
-if [ ! -f $HOME/.vimrc ]; then
-  echo "Linking .vimrc"
-  ln -s $currentdir/vim/vimrc $HOME/.vimrc
-fi
-
-if [ ! -d $HOME/.vim  ]; then
-  echo "Linking .vim"
-  ln -s $currentdir/vim $HOME/.vim
-fi
-
 if [ ! -f $HOME/.tmux.conf ]; then
   echo "Linking .tmux.conf"
   ln -s $currentdir/tmux.conf $HOME/.tmux.conf
-fi
-
-if [ ! -d $HOME/.vim/bundle ]; then
-  echo "Adding bundle dir"
-  mkdir $HOME/.vim/bundle
-fi
-
-if [ ! -f $HOME/.config/nvim/autoload/plug.vim ]; then
-  echo "Installing Vim Plug"
-  curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 if [ ! -f $HOME/.bash_profile ]; then
