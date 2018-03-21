8c14fc2f31ec23ceb37bc91dd71ad1b2fe6e56e8
Jerel Miller <jerelmiller@gmail.com>
2018-03-21 00:30:38 -0700

    USe /Users/jerelmiller/code/dotfiles

--- scripts/symlink_nvim_dotfiles.sh
+++ scripts/symlink_nvim_dotfiles.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-currentdir=$(pwd)
-
 if [ ! -d $HOME/.config/nvim ]; then
   mkdir -p $HOME/.config/nvim
 fi
 
-ln -s $currentdir/config/nvim $HOME/.config/nvim
+ln -s $PWD/config/nvim $HOME/.config/nvim
