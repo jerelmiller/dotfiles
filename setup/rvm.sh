9d96ff40ce7c89beba32b76c1a3edd6c9474f6de
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 01:35:20 -0700

    Install rvm the normal way

--- setup/rvm.sh
+++ setup/rvm.sh
@@ -3,32 +3,10 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" &&
   . ./utils.sh
 
-configure_gpg() {
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-
-  print_result $? "rvm (gpg)"
-}
-
-download_installer() {
-  curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer && \
-    curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer.asc
-
-  print_result $? "rvm (download install)"
-}
-
-verify_gpg() {
-  gpg --verify rvm-installer.asc
-
-  print_result $? "rvm (verify)"
-}
-
-cleanup() {
-  rm -f rvm-installer &> /dev/null
-  rm -f rvm-installer.asc &> /dev/null
-}
+declare -r $RVM_DIRECTORY="$HOME/.rvm"
 
 install_rvm() {
-  bash rvm-installer stable
+  curl -sSL https://get.rvm.io | bash -s stable
 
   print_result $? "rvm (install)"
 }
@@ -43,14 +21,11 @@ main() {
   print_info "rvm"
 
   if ! cmd_exists "rvm"; then
-    configure_gpg
-    download_installer
     install_rvm
   fi
 
+  . $RVM_DIRECTORY/scripts/rvm
   install_latest_ruby
-
-  cleanup
 }
 
 main
