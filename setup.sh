f55b3bc1997c1ae5dd0ec6f67c364938018671b3
Jerel Miller <jerelmiller@gmail.com>
2018-07-19 23:18:48 -0700

    Remove keygen from old setup script

--- setup.sh
+++ setup.sh
@@ -25,15 +25,6 @@ if [ ! -f $HOME/.bash_aliases ]; then
   ln -s $currentdir/bash_aliases $HOME/.bash_aliases
 fi
 
-if [ ! -d $HOME/.ssh ] ||  [ ! -f $HOME/.ssh/id_rsa.pub ]; then
-  echo "Generating ssh key..."
-  ssh-keygen -t rsa -b 4096 -C "jerelmiller@gmail.com"
-
-  if eval "$(ssh-agent -s)" > /dev/null; then
-    ssh-add $HOME/.ssh/id_rsa
-  fi
-fi
-
 echo "Setting up neovim..."
 ./scripts/symlink_nvim_dotfiles.sh
 
