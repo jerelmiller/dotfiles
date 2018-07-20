81635134ed0ea83b8cf31515afd42cfe65c4d814
Jerel Miller <jerelmiller@gmail.com>
2018-07-19 22:50:30 -0700

    add a bunch of util functions

---
+++ setup/utils.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+answer_is_yes() {
+  [[ "$REPLY" =~ ^[Yy]$ ]] \
+    && return 0 \
+    || return 1
+}
+
+ask() {
+  print_question "$1"
+  read -r
+}
+
+ask_for_confirmation() {
+  print_question "$1 (y/n) "
+  read -r -n 1
+  printf "\n"
+}
+
+ask_for_sudo() {
+  # Ask for the administrator password upfront.
+  sudo -v &> /dev/null
+
+  # Update existing `sudo` time stamp
+  # until this script has finished.
+  #
+  # https://gist.github.com/cowboy/3118588
+  while true; do
+      sudo -n true
+      sleep 60
+      kill -0 "$$" || exit
+  done &> /dev/null &
+}
+
+cmd_exists() {
+  command -v "$1" &> /dev/null
+}
+
+print_in_color() {
+  printf "%b" \
+    "$(tput setaf "$2" 2> /dev/null)" \
+    "$1" \
+    "$(tput sgr0 2> /dev/null)"
+}
+
+print_in_green() {
+  print_in_color "$1" 2
+}
+
+print_in_purple() {
+  print_in_color "$1" 5
+}
+
+print_in_red() {
+  print_in_color "$1" 1
+}
+
+print_in_yellow() {
+  print_in_color "$1" 3
+}
+
+print_question() {
+  print_in_yellow "   [?] $1"
+}
+
+print_result() {
+  if [ "$1" -eq 0 ]; then
+    print_success "$2"
+  else
+    print_error "$2"
+  fi
+
+  return "$1"
+}
