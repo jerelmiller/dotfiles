af0c4e2780f4e1fb24195dad9e5740897612a392
Jerel Miller <jerelmiller@gmail.com>
2018-07-19 22:54:42 -0700

    Move the xcode path install out of the bootstrap script and into setup

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -32,11 +32,6 @@ install_xcode_clt() {
     sleep 5
   done
 
-  # Point the `xcode-select` developer directory to
-  # the appropriate directory from within `Xcode.app`
-  # https://github.com/alrra/dotfiles/issues/13
-  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-
   # Prompt to agree to the terms of the Xcode license
   sudo xcodebuild -license
 }
