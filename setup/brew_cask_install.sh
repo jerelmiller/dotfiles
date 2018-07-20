417f2a864cc31a20dc5ea0a46d7adc8be9fcd095
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 00:45:13 -0700

    Add more apps to cask install

--- setup/brew_cask_install.sh
+++ setup/brew_cask_install.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
+# Browsers
+brew cask install google-chrome
+brew cask install firefox
+brew cask install firefox-developer-edition
+brew cask install safari-technology-preview
+brew cask install brave
+
+# Dev
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install visualboyadvance-m
+
+# Misc
+brew cask install 1password
+brew cask install slack
+brew cask install spotify
+brew cask install spectacle
+
+# Fonts
 brew cask install font-hack-nerd-font
