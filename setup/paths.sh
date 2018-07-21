65761d345fc71dac6b33d2d39c114c5eeb1a529c
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 21:30:08 -0700

    Add icloud drive path

--- setup/paths.sh
+++ setup/paths.sh
@@ -3,4 +3,6 @@
 declare -r ASDF_DIRECTORY="$HOME/.asdf"
 declare -r ASDF_SOURCE="$ASDF_DIRECTORY/asdf.sh"
 
+declare -r ICLOUD_DRIVE="$HOME/Library/Mobile\ Documents/com~apple~CloudDocs"
+
 declare -r SYSTEM_RUBY_BIN="/usr/bin/ruby"
