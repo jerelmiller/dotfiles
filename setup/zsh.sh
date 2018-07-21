319ac838109fd509309ade010ca4c8768f17b0f0
Jerel Miller <jerelmiller@gmail.com>
2018-07-21 00:03:47 -0700

    Set permissions on local file

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cd "$(dirname "${BASH_SOURCE[0]}")" && \
-  . ./utils.sh
+  . ./utils.sh && \
+  . ./paths.sh
 
 install_oh_my_zsh() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -9,15 +10,25 @@ install_oh_my_zsh() {
   print_result $? "zsh (install)"
 }
 
+set_login_shell() {
+  chsh -s $(command -v zsh)
+
+  print_success "Set login shell"
+}
+
+create_file() {
+  touch $LOCAL_SHELL_CONFIG_FILE
+  chmod 744 $LOCAL_SHELL_CONFIG_FILE
+
+  print_success "Create local shell config file"
+}
+
 main() {
   print_info "zsh"
 
   install_oh_my_zsh
-
-  # Set zsh as login shell
-  chsh -s $(command -v zsh)
-
-  print_success "Set login shell"
+  set_login_shell
+  create_file
 }
 
 main
