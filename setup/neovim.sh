eb5be7030c818d4fb419885d04a4c13cca128bf0
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 18:48:19 -0700

    Ensure it doesn't install again

--- setup/neovim.sh
+++ setup/neovim.sh
@@ -8,7 +8,9 @@ declare -r VIM_PLUG_LOCATION="$NVIM_DIRECTORY/autoload/plug.vim"
 declare -r VIM_PLUG_REPO_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 install_vim_plug() {
-  curl -fLo $VIM_PLUG_LOCATION --create-dirs $VIM_PLUG_REPO_URL
+  if [ ! -f $VIM_PLUG_LOCATION ]; then
+    curl -fLo $VIM_PLUG_LOCATION --create-dirs $VIM_PLUG_REPO_URL
+  fi
 
   print_result $? "neovim (vim plug)"
 }
