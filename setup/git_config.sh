b8f7414a77abaf357012f7abca6d97af86b4aa68
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 20:55:15 -0700

    Print success

--- setup/git_config.sh
+++ setup/git_config.sh
@@ -7,11 +7,21 @@ print_info "Git config"
 
 git config --global color.ui true
 
+print_success "color"
+
 git config --global user.name "Jerel Miller"
 git config --global user.email "jerelmiller@gmail.com"
 
+print_success "user"
+
 git config --global github.user jerelmiller
+
+print_success "github"
 
 git config --global core.editor nvim
 
+print_success "core"
+
 git config --global alias.graph "log --graph --oneline --decorate"
+
+print_success "alias"
