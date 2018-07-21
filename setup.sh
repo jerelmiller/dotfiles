77e8942de423d473c259af468ae1f3044c227332
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 18:02:22 -0700

    Remove old ruby setup script

--- setup.sh
+++ setup.sh
@@ -40,13 +40,6 @@ echo "Installing Tmux plugin manager..."
 
 brew install elixir
 
-echo "Configuring ruby..."
-
-gem update --system
-gem_install_or_update "bundler"
-number_of_cores=$(sysctl -n hw.ncpu)
-bundle config --global jobs $((number_of_cores - 1))
-
 if [ ! -d $HOME/code/fonts ]; then
   cd $HOME/code
   git clone git@github.com:powerline/fonts.git
