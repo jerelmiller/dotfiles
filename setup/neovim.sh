6dc88e551267486e01bdd363903bb8c989f6f451
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 22:18:02 -0700

    Install neovim from python

--- setup/neovim.sh
+++ setup/neovim.sh
@@ -7,6 +7,12 @@ declare -r NVIM_DIRECTORY="$HOME/.config/nvim"
 declare -r VIM_PLUG_LOCATION="$NVIM_DIRECTORY/autoload/plug.vim"
 declare -r VIM_PLUG_REPO_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
+install_neovim() {
+   pip install neovim
+
+   print_result $? "neovim (install)"
+}
+
 install_vim_plug() {
   if [ ! -f $VIM_PLUG_LOCATION ]; then
     curl -fLo $VIM_PLUG_LOCATION --create-dirs $VIM_PLUG_REPO_URL
@@ -18,6 +24,7 @@ install_vim_plug() {
 main() {
   print_info "neovim"
 
+  install_neovim
   install_vim_plug
 }
 
