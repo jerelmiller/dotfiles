80c7948b5b78bb9be3ce72c526c7687e99b4b8ee
Jerel Miller <jerelmiller@gmail.com>
2018-07-20 21:56:26 -0700

    More settings

--- setup/mac_os.sh
+++ setup/mac_os.sh
@@ -22,11 +22,36 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 print_success "Key repeat"
 
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Finder: Make column-view the preferred view
+defaults write com.apple.finder FXPreferredViewStyle clmv
+
+print_success "Finder"
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+print_success "Dock"
+
+# Avoid creating '.DS_Store' files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
 # Save screenshots in iCloud Drive
 defaults write com.apple.screencapture location $ICLOUD_DRIVE/Screenshots
 
 print_success "Screenshot location"
 
-for app in "Finder"; do
+for app in "Finder" \
+           "Dock" \
+           "SystemUIServer"; do
   killall "${app}" &> /dev/null
 done
