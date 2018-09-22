99a896279e869d5323900e0eb81b4d7c5ae9a111
Jerel Miller <jerelmiller@gmail.com>
2018-09-22 11:37:23 -0700

    Better rbenv config

--- setup/rbenv.sh
+++ setup/rbenv.sh
@@ -2,10 +2,27 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" && . ./utils.sh
 
+initialize() {
+  rbenv init &> /dev/null
+
+  # rbenv init exits with 1 for some reason
+  print_success "rbenv (init)"
+}
+
+add_config() {
+  append_to_shell_config "eval \"\$(rbenv init -)\""
+
+  source_local_config
+
+  print_result $? "rbenv (update $LOCAL_SHELL_CONFIG_FILE)"
+}
+
 main() {
   print_info "rbenv"
 
   source_local_config
-
-  rbenv init
+  initialize
+  add_config
 }
+
+main
