c534b6d29bcdaaaa60074c77a6494203b5c84727
Jerel Miller <jerelmiller@gmail.com>
2019-01-29 22:04:24 -0800

    Symlink vscode files

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -2,12 +2,41 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" && . ./utils.sh
 
-main() {
+declare -a VSCODE_DIR="$HOME/Library/Application Support/Code/User"
+
+declare -a FILES_TO_SYMLINK=(
+  "vscode/settings.json"
+  "vscode/keybindings.json"
+  "vscode/snippets/"
+)
+
+setup_key_repeating() {
   # Enable key repeating https://github.com/VSCodeVim/Vim#mac-setup
   defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
   defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
 
   print_result $? "vscode (enable key repeating)"
+}
+
+symlink_settings_file() {
+  local name="$1"
+  local source_file="$(cd .. && pwd)/$name"
+  local target_file="$VSCODE_DIR/$(basename "$name")"
+
+  maybe_symlink_file "$source_file" "$target_file"
+}
+
+symlink_settings() {
+  print_info "Symlink settings"
+
+  for i in ${FILES_TO_SYMLINK[@]}; do
+    symlink_settings_file $i
+  done
+}
+
+main() {
+  setup_key_repeating
+  symlink_settings
 }
 
 main
