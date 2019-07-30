bbbc8d31feaff9c3c0c28aff11cf6ecb65a6a275
Jerel Miller <jmiller@newrelic.com>
2019-07-29 23:23:27 -0700

    Add pam reattach

--- setup/sudo_touch_id.sh
+++ setup/sudo_touch_id.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Paste the following into /etc/pam.d/sudo"
-echo "\tauth sufficient pam_tid.so"
+echo "\tauth     optional     pam_reattach.so"
+echo "\tauth     sufficient   pam_tid.so"
