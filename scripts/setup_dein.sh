c90f3732850f475db63cd8914c557569c2712e47
Jerel Miller <jerelmiller@gmail.com>
2020-07-13 22:53:14 -0700

    Remove dein

--- scripts/setup_dein.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
- curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
- chmod +x ./installer.sh
- ./installer.sh ~/.config/nvim/dein/
- rm -i installer.sh
