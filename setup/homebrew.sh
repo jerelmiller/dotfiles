8b991f531aacb5c79f61fd20479f366ddcd5d82e
Jerel Miller <jerelmiller@gmail.com>
2022-02-19 01:04:13 -0800

    Update to newest homebrew install command

--- setup/homebrew.sh
+++ setup/homebrew.sh
@@ -17,7 +17,7 @@ get_homebrew_git_config_file_path() {
 
 install_homebrew() {
   if ! cmd_exists "brew"; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
   print_result $? "Homebrew"
