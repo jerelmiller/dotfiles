0a33881da223be17e33c9536dac280106ff3b4de
Jerel Miller <jerelmiller@gmail.com>
2023-01-06 13:35:13 -0700

    Comment out term setup for now

--- setup/term.sh
+++ setup/term.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+
 cd "$(dirname "${BASH_SOURCE[0]}")" &&
   . ./utils.sh
 
-declare -r BASE_DIR=$(cd .. && pwd)
 
-main() {
-  print_info "term"
+print_warning "Follow this guide: https://dev.to/jibundit/undercurl-display-on-neovim-and-tmux-with-iterm2-3pi0"
 
-  tic $BASE_DIR/tmux-256color.terminfo
-  print_result $? "tmux-256color"
+# declare -r BASE_DIR=$(cd .. && pwd)
 
-  tic $BASE_DIR/tmux-256color-italic.terminfo
-  print_result $? "tmux-256color-italic"
-}
+# main() {
+#   print_info "term"
 
-main
+#   tic $BASE_DIR/tmux-256color.terminfo
+#   print_result $? "tmux-256color"
+
+#   tic $BASE_DIR/tmux-256color-italic.terminfo
+#   print_result $? "tmux-256color-italic"
+# }
+
+# main
