1130628e955f6761704f1fe90c02325eabdc0315
Jerel Miller <jerelmiller@gmail.com>
2023-01-06 02:44:24 -0700

    Ensure lsp is completion

--- config/nvim/lua/user/cmp.lua
+++ config/nvim/lua/user/cmp.lua
@@ -44,6 +44,7 @@ cmp.setup({
     end, { 'i', 's' }),
   }),
   sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
     { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
