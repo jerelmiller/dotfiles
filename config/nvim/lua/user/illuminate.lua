e26207d5877dfc703d68a0b23308f5981b1161c2
Jerel Miller <jerelmiller@gmail.com>
2023-01-06 03:45:52 -0700

    Add illuminate colors

---
+++ config/nvim/lua/user/illuminate.lua
@@ -0,0 +1,3 @@
+vim.cmd.highlight({ 'link', 'IlluminatedWordText', 'CursorLine' })
+vim.cmd.highlight({ 'link', 'IlluminatedWordRead', 'CursorLine' })
+vim.cmd.highlight({ 'link', 'IlluminatedWordWrite', 'CursorLine' })
