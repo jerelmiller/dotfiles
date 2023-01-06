2b72010c29c50ef91fcf401967aaefb78669d022
Jerel Miller <jerelmiller@gmail.com>
2023-01-06 02:44:41 -0700

    Remove extra whitespace

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -85,19 +85,19 @@ packer.startup(function(use)
   use 'RRethy/nvim-treesitter-endwise'
 
   -- Fuzzy Finder (files, lsp, etc)
-  use { 
+  use {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    requires = { 
-      'nvim-lua/plenary.nvim' 
-    } 
+    requires = {
+      'nvim-lua/plenary.nvim'
+    }
   }
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 
+  use {
     'nvim-telescope/telescope-fzf-native.nvim',
     run = 'make',
-    cond = vim.fn.executable 'make' == 1 
+    cond = vim.fn.executable 'make' == 1
   }
 
   -- Color schemes
