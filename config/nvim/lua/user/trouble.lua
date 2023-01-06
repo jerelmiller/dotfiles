6b4ee21aa4386ec34f8112bfb65d5e2429ca2578
Jerel Miller <jerelmiller@gmail.com>
2023-01-06 10:33:19 -0700

    Fix diagnostic signs

--- config/nvim/lua/user/trouble.lua
+++ config/nvim/lua/user/trouble.lua
@@ -1,1 +1,15 @@
 require('trouble').setup()
+
+local signs = {
+  Error = "",
+  Warn = "",
+  Hint = "",
+  Info = "",
+}
+
+ for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+ end
+
+
