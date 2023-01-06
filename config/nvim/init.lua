916d765761385776f6761b266819798b2587ab33
Jerel Miller <jerelmiller@gmail.com>
2023-01-06 01:49:10 -0700

    Create an init.lua file

---
+++ config/nvim/init.lua
@@ -0,0 +1,8 @@
+require('user.options')
+require('user.keymaps')
+require('user.plugins')
+require('user.colorscheme')
+require('user.cmp')
+require('user.treesitter')
+require('user.autopairs')
+require('user.comment')
