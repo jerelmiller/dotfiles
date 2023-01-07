47a9fb2d8826148ea5aeb58b136e6cf2cb54264b
Jerel Miller <jerelmiller@gmail.com>
2023-01-06 17:35:14 -0700

    Better colors and icons for completion menu

--- config/nvim/lua/user/cmp.lua
+++ config/nvim/lua/user/cmp.lua
@@ -7,10 +7,64 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+local cmp_kinds = {
+  Text = '  ',
+  Method = '  ',
+  Function = '  ',
+  Constructor = '  ',
+  Field = '  ',
+  Variable = '  ',
+  Class = '  ',
+  Interface = '  ',
+  Module = '  ',
+  Property = '  ',
+  Unit = '  ',
+  Value = '  ',
+  Enum = '  ',
+  Keyword = '  ',
+  Snippet = '  ',
+  Color = '  ',
+  File = '  ',
+  Reference = '  ',
+  Folder = '  ',
+  EnumMember = '  ',
+  Constant = '  ',
+  Struct = '  ',
+  Event = '  ',
+  Operator = '  ',
+  TypeParameter = '  ',
+}
+
+vim.cmd [[
+  highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#4C566A
+  highlight! link CmpItemAbbrMatch Statement
+
+  highlight! link CmpItemAbbrMatchFuzzy CmpItemAbbrMatch
+
+  highlight! link CmpItemKindVariable Variable
+  highlight! link CmpItemKindInterface CmpItemKindVariable
+  highlight! link CmpItemKindText CmpItemKindVariable
+ 
+  highlight! link CmpItemKindFunction Function
+  highlight! link CmpItemKindMethod CmpItemKindFunction
+ 
+  highlight! link CmpItemKindKeyword Keyword
+  highlight! link CmpItemKindProperty CmpItemKindKeyword
+  highlight! link CmpItemKindUnit CmpItemKindKeyword
+
+  highlight! link CmpItemKindClass SpecialChar
+]]
+
 cmp.setup({
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
+    end
+  },
+  formatting = {
+    format = function(_, vim_item)
+      vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+      return vim_item
     end
   },
   mapping = cmp.mapping.preset.insert({
