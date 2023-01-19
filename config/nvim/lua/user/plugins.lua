b14e07b9682fa8004db0d73e07b0ac3f4f272dfd
Jerel Miller <jerelmiller@gmail.com>
2023-01-18 22:29:17 -0700

    Add todo-comments

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -171,6 +171,13 @@ packer.startup(function(use)
   use('nvim-telescope/telescope-ui-select.nvim')
   use('alexghergh/nvim-tmux-navigation')
 
+  use({
+    'folke/todo-comments.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+    },
+  })
+
   -- Color schemes
   use('arcticicestudio/nord-vim')
 
