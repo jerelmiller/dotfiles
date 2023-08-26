4c2042b34f223b721ec3cae427f65f5236c2bd4c
Jerel Miller <jerelmiller@gmail.com>
2023-08-25 23:17:25 -0600

    Add treesitter and comment

--- config/nvim/lua/jerel/lazy.lua
+++ config/nvim/lua/jerel/lazy.lua
@@ -1,5 +1,4 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     'git',
@@ -10,14 +9,29 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
+-- Set leader before plugins to ensure correct mappings
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
 require('lazy').setup({
+  {
+    'arcticicestudio/nord-vim',
+    lazy = false,
+    priority = 1000
+  },
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
-  'arcticicestudio/nord-vim',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+  },
+  {
+    'numToStr/Comment.nvim',
+    lazy = false,
+  }
 })
