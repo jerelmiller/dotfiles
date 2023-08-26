d1ee3a8ba92271535830d3a76016469c665999fe
Jerel Miller <jerelmiller@gmail.com>
2023-08-26 02:03:52 -0600

    remove trailing whitespace

--- config/nvim/after/queries/tsx/highlights.scm
+++ config/nvim/after/queries/tsx/highlights.scm
@@ -1,36 +1,36 @@
 ; extends
 
-(jsx_opening_element 
+(jsx_opening_element
   name: (identifier) @jsx.tag
-) 
+)
 
-(jsx_opening_element 
+(jsx_opening_element
   name: (member_expression (identifier) @jsx.tag (property_identifier) @jsx.tag)
 )
 
-(jsx_opening_element 
+(jsx_opening_element
   ((identifier) @jsx.component (#match? @jsx.component "^[A-Z]"))
 )
 
-(jsx_opening_element 
+(jsx_opening_element
   (member_expression
     ((identifier) @jsx.component (#match? @jsx.component "^[A-Z]") (property_identifier) @jsx.component)
   )
 )
 
-(jsx_closing_element 
+(jsx_closing_element
   name: (identifier) @jsx.tag
-) 
+)
 
-(jsx_closing_element 
+(jsx_closing_element
   (member_expression (identifier) @jsx.tag (property_identifier) @jsx.tag)
 )
 
-(jsx_closing_element 
+(jsx_closing_element
   ((identifier) @jsx.component (#match? @jsx.component "^[A-Z]"))
 )
 
-(jsx_closing_element 
+(jsx_closing_element
   (member_expression
     ((identifier) @jsx.component (#match? @jsx.component "^[A-Z]") (property_identifier) @jsx.component)
   )
@@ -40,7 +40,7 @@
   (member_expression (identifier) "." @jsx.delimiter)
 )
 
-(jsx_closing_element 
+(jsx_closing_element
   (member_expression (identifier) "." @jsx.delimiter)
 )
 
