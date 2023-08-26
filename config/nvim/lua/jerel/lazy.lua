daee45885ea71afd814e5d96d4188565e6519531
Jerel Miller <jerelmiller@gmail.com>
2023-08-25 22:24:19 -0600

    Install lazy.nvim and move telescope/nord to it

---
+++ config/nvim/lua/jerel/lazy.lua
@@ -0,0 +1,23 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  'arcticicestudio/nord-vim',
+})
