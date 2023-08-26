03898e7c0085fba910f13cfeba8a1124887bcce8
Jerel Miller <jerelmiller@gmail.com>
2023-08-26 00:41:36 -0600

    Add diagnostic config

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -20,3 +20,31 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
 
+vim.diagnostic.config({
+  virtual_text = {
+    format = function(diagnostic)
+      return string.format(
+        '[%s] %s (%s)',
+        diagnostic.source,
+        diagnostic.message,
+        diagnostic.code
+      )
+    end,
+  },
+  float = {
+    format = function(diagnostic)
+      return string.format('[%s] %s', diagnostic.source, diagnostic.message)
+    end,
+    border = 'rounded',
+  },
+})
+
+vim.lsp.handlers['textDocument/hover'] =
+  vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+
+vim.lsp.handlers['textDocument/signatureHelp'] =
+  vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
+
+require('lspconfig.ui.windows').default_options = {
+  border = 'rounded',
+}
