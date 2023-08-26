c2dfe2ebc8ce4c9f520195c48870d5ab6336862c
Jerel Miller <jerelmiller@gmail.com>
2023-08-25 23:42:12 -0600

    Add lsp-zero and trouble

--- config/nvim/lua/jerel/lazy.lua
+++ config/nvim/lua/jerel/lazy.lua
@@ -33,5 +33,24 @@ require('lazy').setup({
   {
     'numToStr/Comment.nvim',
     lazy = false,
+  },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   }
 })
