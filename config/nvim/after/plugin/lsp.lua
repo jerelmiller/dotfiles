cf4e683f27e63a23892b348f27c336500a7655aa
Jerel Miller <jerelmiller@gmail.com>
2023-08-26 00:35:17 -0600

    Move lsp config to proper location

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,12 +1,22 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
 end)
+
+lsp.setup_servers({
+  'cssls',
+  'cssmodules_ls',
+  'elixirls',
+  'html',
+  'graphql',
+  'lua_ls',
+  'tailwindcss',
+  'tsserver',
+})
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
