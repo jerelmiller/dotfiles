4e3f055bdb070bdf63062305e80db9e6ba27a12a
Jerel Miller <jerelmiller@gmail.com>
2023-09-06 17:15:49 -0600

    Comment out git config setup for now

--- setup/git_config.sh
+++ setup/git_config.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
-cd "$(dirname "${BASH_SOURCE[0]}")" &&
-  . ./utils.sh
+# cd "$(dirname "${BASH_SOURCE[0]}")" &&
+#   . ./utils.sh
 
-print_info "Git config"
+# print_info "Git config"
 
-git config --global color.ui true
+# git config --global color.ui true
 
-git config --global user.name "Jerel Miller"
-git config --global user.email "jerelmiller@gmail.com"
+# git config --global user.name "Jerel Miller"
+# git config --global user.email "jerelmiller@gmail.com"
 
-git config --global github.user jerelmiller
+# git config --global github.user jerelmiller
 
-git config --global core.editor nvim
-git config --global core.pager "delta --diff-so-fancy"
+# git config --global core.editor nvim
+# git config --global core.pager "delta --diff-so-fancy"
 
-git config --global alias.graph "log --graph --oneline --decorate"
+# git config --global alias.graph "log --graph --oneline --decorate"
 
-git config --global interactive.diffFilter 'delta --color-only'
+# git config --global interactive.diffFilter 'delta --color-only'
 
-# Mark any lines that have had a commit skipped using --ignore-rev with a `?`
-git config --global blame.markIgnoredLines true
-# Mark any lines that were added in a skipped commit and can not be attributed with a `*`
-git config --global blame.markUnblamableLines true
+# # Mark any lines that have had a commit skipped using --ignore-rev with a `?`
+# git config --global blame.markIgnoredLines true
+# # Mark any lines that were added in a skipped commit and can not be attributed with a `*`
+# git config --global blame.markUnblamableLines true
 
 
-git config --global delta.navigate true
-git config --global delta.line-numbers true
-git config --global delta.light false
-git config --global delta.true-color "always"
-git config --global delta.theme "Nord"
-git config --global delta.line-numbers-zero-style "#4C566A"
-git config --global delta.line-numbers-minus-style "#BF616A"
-git config --global delta.line-numbers-plus-style "#A3BE8C"
+# git config --global delta.navigate true
+# git config --global delta.line-numbers true
+# git config --global delta.light false
+# git config --global delta.true-color "always"
+# git config --global delta.theme "Nord"
+# git config --global delta.line-numbers-zero-style "#4C566A"
+# git config --global delta.line-numbers-minus-style "#BF616A"
+# git config --global delta.line-numbers-plus-style "#A3BE8C"
 
-git config --global diff.colorMoved "default"
+# git config --global diff.colorMoved "default"
 
-git config --global merge.conflictstyle "diff3"
+# git config --global merge.conflictstyle "diff3"
 
-print_success "Git config"
+# print_success "Git config"
