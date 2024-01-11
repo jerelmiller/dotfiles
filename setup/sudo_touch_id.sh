bf98c86cb008a48230c52787121420818783e317
Jerel Miller <jerelmiller@gmail.com>
2024-01-10 20:48:56 -0700

    Remove all setup scripts

--- setup/sudo_touch_id.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Paste the following into /etc/pam.d/sudo"
-echo "\tauth     optional     pam_reattach.so"
-echo "\tauth     sufficient   pam_tid.so"
--- setup/xcode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Point the `xcode-select` developer directory to
-# the appropriate directory from within `Xcode.app`
-# https://github.com/alrra/dotfiles/issues/13
-sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
