938a8724d3cdc971962f7d41c8ccd266308561e4
Jerel Miller <jerelmiller@gmail.com>
2024-01-12 20:51:44 -0700

    Add keybindings for trouble

--- nvim/.config/nvim/lua/jerel/plugins/diagnostics.lua
+++ nvim/.config/nvim/lua/jerel/plugins/diagnostics.lua
@@ -8,7 +8,9 @@ return {
       vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
       vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
       vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-      vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end) 
+      vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+      vim.keymap.set("n", "]d", function() require("trouble").next({ skip_groups = true, jump = true }) end)
+      vim.keymap.set("n", "[d", function() require("trouble").previous({ skip_groups = true, jump = true }) end)
     end
   }
 }
