128895452a68c3df28c5fd66a6657a6393301c15
Jerel Miller <jerelmiller@gmail.com>
2024-01-13 00:22:21 -0700

    Add conform to save on format

---
+++ nvim/.config/nvim/lua/jerel/plugins/formatting.lua
@@ -0,0 +1,17 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        elixir = { "mix" },
+        lua = { "stylua" },
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } }
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 500
+      }
+    })
+  end
+}
