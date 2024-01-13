da354b9bf30a5e39da33994260fa0eb549edb428
Jerel Miller <jerelmiller@gmail.com>
2024-01-12 23:19:30 -0700

    Minor tweak to syntax

--- nvim/.config/nvim/lua/jerel/init.lua
+++ nvim/.config/nvim/lua/jerel/init.lua
@@ -37,7 +37,7 @@ autocmd('LspAttach', {
     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
+      vim.lsp.buf.format({ async = true })
+    end)
   end,
 })
