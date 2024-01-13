a5acbf0944a69fd22190cc028f372c49b1d57f8e
Jerel Miller <jerelmiller@gmail.com>
2024-01-12 20:46:51 -0700

    Create autocmd in init.lua

--- nvim/.config/nvim/lua/jerel/init.lua
+++ nvim/.config/nvim/lua/jerel/init.lua
@@ -1,3 +1,43 @@
 require("jerel.set")
 require("jerel.remap")
 require("jerel.lazy")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local JerelGroup = augroup("Jerel", {})
+
+autocmd({ "BufWritePre" }, {
+  group = JerelGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+autocmd('LspAttach', {
+  group = JerelGroup,
+  callback = function(e)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[e.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = e.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
