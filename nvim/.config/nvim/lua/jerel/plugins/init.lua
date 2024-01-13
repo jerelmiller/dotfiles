6eee92b2f749710c33f56ed4237a2f87890e5988
Jerel Miller <jerelmiller@gmail.com>
2024-01-12 23:23:09 -0700

    Add keymap for git

--- nvim/.config/nvim/lua/jerel/plugins/init.lua
+++ nvim/.config/nvim/lua/jerel/plugins/init.lua
@@ -1,5 +1,10 @@
 return {
-  "tpope/vim-fugitive",
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+    end
+  },
   {
     "mbbill/undotree",
     config = function()
