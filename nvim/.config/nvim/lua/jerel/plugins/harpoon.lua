2a3bf61562793856a71a53dad35885d427d89a7e
Jerel Miller <jerelmiller@gmail.com>
2024-01-15 08:51:47 -0700

    Update keymaps for harpoon

--- nvim/.config/nvim/lua/jerel/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/jerel/plugins/harpoon.lua
@@ -7,9 +7,9 @@ return {
 
     harpoon:setup({})
 
-    vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
-    vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
   end
 }
