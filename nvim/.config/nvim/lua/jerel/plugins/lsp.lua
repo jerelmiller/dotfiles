f5953d73b99f9e0187585a76edd7591af70ea786
Jerel Miller <jerelmiller@gmail.com>
2024-01-15 09:16:47 -0700

    Add borders for lsp

--- nvim/.config/nvim/lua/jerel/plugins/lsp.lua
+++ nvim/.config/nvim/lua/jerel/plugins/lsp.lua
@@ -36,7 +36,11 @@ return {
         handlers = {
           function(server_name)
             require('lspconfig')[server_name].setup({
-              capabilities = capabilities
+              capabilities = capabilities,
+              handlers = {
+                ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+                ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+              }
             })
           end
         }
