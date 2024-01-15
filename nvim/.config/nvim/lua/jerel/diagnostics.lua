ad036f9dda35f6b0786816e88839108e85a4e1a4
Jerel Miller <jerelmiller@gmail.com>
2024-01-15 10:15:20 -0700

    Minor changes to diagnostics

--- nvim/.config/nvim/lua/jerel/diagnostics.lua
+++ nvim/.config/nvim/lua/jerel/diagnostics.lua
@@ -1,10 +1,17 @@
 local signs = require("jerel.signs")
 
 vim.diagnostic.config({
-  float = { border = "rounded" },
+  float = {
+    border = "rounded",
+    source = "always"
+  },
+  virtual_text = {
+    prefix = "●",
+    source = "if_many"
+  },
 })
 
-for type, icon in pairs(signs) do
+for type, icon in pairs(signs.diagnostic) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
