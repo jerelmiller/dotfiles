548768bb07ee554a1ca59551d7482cf46f7838ff
Jerel Miller <jerelmiller@gmail.com>
2024-01-15 09:16:56 -0700

    Disable warning for missing fields

--- nvim/.config/nvim/lua/jerel/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/jerel/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup({
         ensure_installed = {
           'bash',
