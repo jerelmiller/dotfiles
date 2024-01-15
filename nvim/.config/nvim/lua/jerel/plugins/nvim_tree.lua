780d1a940c6d3aac29110e0e5da9597680c7c62e
Jerel Miller <jerelmiller@gmail.com>
2024-01-15 11:40:09 -0700

    Add nvim tree

---
+++ nvim/.config/nvim/lua/jerel/plugins/nvim_tree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  config = function()
+    require("nvim-tree").setup()
+    local api = require("nvim-tree.api")
+
+    vim.keymap.set("n", "<C-n>", function() api.tree.toggle({ find_file = true, focus = true }) end)
+    vim.keymap.set("n", "<leader>nt", vim.cmd.NvimTreeToggle)
+  end
+}
