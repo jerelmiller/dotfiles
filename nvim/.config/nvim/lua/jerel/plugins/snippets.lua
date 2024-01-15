ddbfbafa45150222a3ea9005cf68cddf40d6ac3c
Jerel Miller <jerelmiller@gmail.com>
2024-01-15 08:42:45 -0700

    Add luasnip keymaps

--- nvim/.config/nvim/lua/jerel/plugins/snippets.lua
+++ nvim/.config/nvim/lua/jerel/plugins/snippets.lua
@@ -1,5 +1,18 @@
 return {
   "L3MON4D3/LuaSnip",
   version = "v2.*",
-  build = "make install_jsregexp"
+  build = "make install_jsregexp",
+  config = function()
+    local ls = require('luasnip')
+
+    vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+
+    vim.keymap.set({ "i", "s" }, "<C-E>", function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end, { silent = true })
+  end
 }
