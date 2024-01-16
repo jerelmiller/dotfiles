734af1b165bc08d66a62eceee7198d17326de7eb
Jerel Miller <jerelmiller@gmail.com>
2024-01-16 12:59:07 -0700

    Some tweaks to colors

--- nvim/.config/nvim/lua/jerel/plugins/colors.lua
+++ nvim/.config/nvim/lua/jerel/plugins/colors.lua
@@ -1,8 +1,24 @@
 return {
   "catppuccin/nvim",
-  name = "catppuccin", 
+  name = "catppuccin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      highlight_overrides = {
+        macchiato = function(colors)
+          return {
+            ["@parameter"] = { fg = colors.rosewater },
+            ["@punctuation.bracket"] = { fg = colors.red },
+            ["@tag.attribute.tsx"] = { fg = colors.sky },
+            ["@tag.delimiter"] = { fg = colors.maroon },
+            ["@tag.tsx"] = { fg = colors.yellow },
+            Comment = { fg = colors.surface1 },
+            Operator = { fg = colors.subtext0 },
+          }
+        end
+      },
+    })
+
     vim.cmd.colorscheme("catppuccin-macchiato")
   end
 }
