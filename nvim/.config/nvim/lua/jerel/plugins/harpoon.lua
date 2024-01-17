ec0e1489ba05acf86b7ec1ba09ca2c1a78f45205
Jerel Miller <jerelmiller@gmail.com>
2024-01-17 01:02:48 -0700

    Add better keymaps to move between harpoon files

--- nvim/.config/nvim/lua/jerel/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/jerel/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
 
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<S-p>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<S-n>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "]f", function() harpoon:list():next() end)
+    vim.keymap.set("n", "[f", function() harpoon:list():prev() end)
   end
 }
