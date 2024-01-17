3fb54718b1fb89f8f787df051bb3afc283e0b3b1
Jerel Miller <jerelmiller@gmail.com>
2024-01-16 23:56:23 -0700

    Define more color overrides

--- nvim/.config/nvim/lua/jerel/plugins/colors.lua
+++ nvim/.config/nvim/lua/jerel/plugins/colors.lua
@@ -7,6 +7,10 @@ return {
       highlight_overrides = {
         macchiato = function(colors)
           return {
+            ["@constant.builtin.typescript"] = { link = "Keyword" },
+            ["@lsp.type.class.typescript"] = { fg = colors.sky },
+            ["@lsp.typemod.class.declaration.typescript"] = { fg = colors.blue },
+            ["@lsp.typemod.function.defaultLibrary.typescript"] = { fg = colors.yellow },
             ["@parameter"] = { fg = colors.rosewater },
             ["@punctuation.bracket"] = { fg = colors.red },
             ["@tag.attribute.tsx"] = { fg = colors.sky },
@@ -14,7 +18,12 @@ return {
             ["@tag.tsx"] = { fg = colors.yellow },
             Comment = { fg = colors.surface1 },
             DiagnosticUnderlineError = { undercurl = true },
+            DiagnosticUnderlineHint = { undercurl = true },
+            DiagnosticUnderlineInfo = { undercurl = true },
+            DiagnosticUnderlineWarn = { undercurl = true },
+            DiagnosticUnnecessary = { fg = colors.surface2, sp = colors.yellow, undercurl = true, italic = true },
             Operator = { fg = colors.subtext0 },
+            Type = { fg = colors.rosewater }
           }
         end
       },
