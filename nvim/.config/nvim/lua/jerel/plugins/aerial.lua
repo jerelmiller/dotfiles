db6f54263eccb8bd0a74c9b20e9a7bd326d2df21
Jerel Miller <jerelmiller@gmail.com>
2024-01-17 01:03:14 -0700

    Some aerial config

--- nvim/.config/nvim/lua/jerel/plugins/aerial.lua
+++ nvim/.config/nvim/lua/jerel/plugins/aerial.lua
@@ -5,8 +5,15 @@ return {
     "nvim-tree/nvim-web-devicons"
   },
   config = function()
-    require("aerial").setup()
+    require("aerial").setup({
+      disable_max_lines = 20000,
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end
+    })
 
-    vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle!<CR>")
+    vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle! left<CR>")
   end
 }
