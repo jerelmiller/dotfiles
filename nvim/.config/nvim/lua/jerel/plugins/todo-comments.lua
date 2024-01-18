9d684114a1d3ee1047c0dca67b470f99393a59c0
Jerel Miller <jerelmiller@gmail.com>
2024-01-17 23:07:12 -0700

    Add todo-comments

---
+++ nvim/.config/nvim/lua/jerel/plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local todo = require("todo-comments")
+
+    todo.setup()
+
+    vim.keymap.set("n", "]t", function() todo.jump_next() end)
+    vim.keymap.set("n", "[t", function() todo.jump_prev() end)
+  end
+}
