804fa73126fdc7bfc9b6b16f33b58fe2ffdf64d5
Jerel Miller <jerelmiller@gmail.com>
2024-01-17 23:29:16 -0700

    Use local var for trouble setup

--- nvim/.config/nvim/lua/jerel/plugins/trouble.lua
+++ nvim/.config/nvim/lua/jerel/plugins/trouble.lua
@@ -3,14 +3,16 @@ return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-      vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-      vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-      vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-      vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-      vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
-      vim.keymap.set("n", "]d", function() require("trouble").next({ skip_groups = true, jump = true }) end)
-      vim.keymap.set("n", "[d", function() require("trouble").previous({ skip_groups = true, jump = true }) end)
+      local trouble = require("trouble")
+
+      vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
+      vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end)
+      vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
+      vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
+      vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
+      vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
+      vim.keymap.set("n", "]d", function() trouble.next({ skip_groups = true, jump = true }) end)
+      vim.keymap.set("n", "[d", function() trouble.previous({ skip_groups = true, jump = true }) end)
     end
   }
 }
