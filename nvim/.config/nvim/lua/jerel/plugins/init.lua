7939c866a75254816e8723645603c7c306aa10cf
Jerel Miller <jerelmiller@gmail.com>
2024-01-21 14:58:19 -0700

    Move mini.comment config to own file

--- nvim/.config/nvim/lua/jerel/plugins/init.lua
+++ nvim/.config/nvim/lua/jerel/plugins/init.lua
@@ -2,14 +2,6 @@ return {
   "fladson/vim-kitty",
   "tpope/vim-abolish",
   {
-    "echasnovski/mini.comment",
-    opts = {
-      options = {
-        ignore_blank_line = true,
-      },
-    },
-  },
-  {
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
---
+++ nvim/.config/nvim/lua/jerel/plugins/mini-comment.lua
@@ -0,0 +1,8 @@
+return {
+  "echasnovski/mini.comment",
+  opts = {
+    options = {
+      ignore_blank_line = true,
+    },
+  },
+}
