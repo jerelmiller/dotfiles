89bb7127dbcd67847dae8d8fd08dacc2154059f7
Jerel Miller <jerelmiller@gmail.com>
2024-01-21 15:00:06 -0700

    Move vim-fugitive to own file

--- nvim/.config/nvim/lua/jerel/plugins/init.lua
+++ nvim/.config/nvim/lua/jerel/plugins/init.lua
@@ -1,10 +1,4 @@
 return {
   "fladson/vim-kitty",
   "tpope/vim-abolish",
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    end,
-  },
 }
