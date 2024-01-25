ab657e4bfcfaa9139e3b7e670f2f50c3dbfcfd50
Jerel Miller <jerelmiller@gmail.com>
2024-01-25 09:28:49 -0700

    Filter out "No information available" messages in notify

--- nvim/.config/nvim/lua/jerel/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/jerel/plugins/nvim-notify.lua
@@ -1,6 +1,20 @@
 return {
   "rcarriga/nvim-notify",
+  event = "VeryLazy",
   config = function()
-    vim.notify = require("notify")
+    local notify = require("notify")
+    local filtered_messages = { "No information available" }
+
+    -- Override notify function to filter out messages
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.notify = function(message, level, opts)
+      for _, msg in ipairs(filtered_messages) do
+        if message == msg then
+          return
+        end
+      end
+
+      return notify(message, level, opts)
+    end
   end,
 }
