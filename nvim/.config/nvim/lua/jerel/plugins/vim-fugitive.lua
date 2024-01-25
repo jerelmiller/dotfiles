2441b599d0a941f348af11b9e93710ab8a78d371
Jerel Miller <jerelmiller@gmail.com>
2024-01-25 16:26:09 -0700

    Add map to push to remote

--- nvim/.config/nvim/lua/jerel/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/jerel/plugins/vim-fugitive.lua
@@ -2,5 +2,8 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    vim.keymap.set("n", "<leader>gp", function()
+      vim.cmd("Git push")
+    end)
   end,
 }
