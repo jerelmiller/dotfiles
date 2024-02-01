b8639864275b179600dcb1b1009fae7c77b35933
Jerel Miller <jerelmiller@gmail.com>
2024-02-01 15:29:11 -0700

    Add keymaps for neotest

--- nvim/.config/nvim/lua/jerel/plugins/neotest.lua
+++ nvim/.config/nvim/lua/jerel/plugins/neotest.lua
@@ -7,8 +7,10 @@ return {
     "nvim-neotest/neotest-jest",
   },
   config = function()
+    local neotest = require("neotest")
+
     ---@diagnostic disable-next-line: missing-fields
-    require("neotest").setup({
+    neotest.setup({
       adapters = {
         require("neotest-jest")({
           jestCommand = "npm test --",
@@ -24,5 +26,33 @@ return {
         enabled = false,
       },
     })
+
+    vim.keymap.set("n", "<leader>sf", function()
+      neotest.run.run(vim.fn.expand("%"))
+    end, { desc = "Test file" })
+    vim.keymap.set("n", "<leader>sl", function()
+      neotest.run.run_last()
+    end, { desc = "Test last" })
+    vim.keymap.set("n", "<leader>sn", function()
+      neotest.run.run()
+    end, { desc = "Test nearest" })
+    vim.keymap.set("n", "<leader>st", function()
+      neotest.summary.toggle()
+    end, { desc = "Toggle test summary" })
+    vim.keymap.set("n", "<leader>so", function()
+      neotest.output.open({ enter = true })
+    end, { desc = "Open test output" })
+    vim.keymap.set("n", "]s", function()
+      neotest.jump.next()
+    end, { desc = "Jump to next test" })
+    vim.keymap.set("n", "[s", function()
+      neotest.jump.prev()
+    end, { desc = "Jump to previous test" })
+    vim.keymap.set("n", "]x", function()
+      neotest.jump.next({ status = "failed" })
+    end, { desc = "Jump to next failed test" })
+    vim.keymap.set("n", "[x", function()
+      neotest.jump.prev({ status = "failed" })
+    end, { desc = "Jump to previous failed test" })
   end,
 }
