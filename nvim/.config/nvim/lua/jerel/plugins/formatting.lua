d593a28fa8ef5d705a08b2c9f2d13ff90b9d32c9
Jerel Miller <jerelmiller@gmail.com>
2024-02-07 19:13:55 -0700

    Format css with prettier

--- nvim/.config/nvim/lua/jerel/plugins/formatting.lua
+++ nvim/.config/nvim/lua/jerel/plugins/formatting.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
+        css = { { "prettierd", "prettier" } },
         elixir = { "mix" },
         lua = { "stylua" },
         javascript = { { "prettierd", "prettier" } },
