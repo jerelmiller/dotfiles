48bc335e40668d7726f4848f7f7b749cd2ba1618
Jerel Miller <jerelmiller@gmail.com>
2024-05-28 16:40:15 -0600

    Supress projects v2 scope notification

--- nvim/.config/nvim/lua/jerel/plugins/octo.lua
+++ nvim/.config/nvim/lua/jerel/plugins/octo.lua
@@ -6,6 +6,10 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("octo").setup()
+    require("octo").setup({
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
+    })
   end,
 }
