77dd996be45db0c4729604a3c5b1015351202006
Jerel Miller <jerelmiller@gmail.com>
2024-08-13 21:46:21 -0600

    Update config for conform

--- nvim/.config/nvim/lua/jerel/plugins/formatting.lua
+++ nvim/.config/nvim/lua/jerel/plugins/formatting.lua
@@ -2,21 +2,22 @@ return {
   "stevearc/conform.nvim",
   config = function()
     require("conform").setup({
-      async = true,
       formatters_by_ft = {
-        css = { { "prettierd", "prettier" } },
+        css = { "prettierd", "prettier" },
         elixir = { "mix" },
         lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        json = { { "prettierd", "prettier" } },
-        jsonc = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
+        javascript = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        json = { "prettierd", "prettier" },
+        jsonc = { "prettierd", "prettier" },
+        typescript = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        timeout_ms = 2000,
+      default_format_opts = {
+        stop_after_first = true,
+      },
+      format_after_save = {
+        lsp_format = "fallback",
       },
     })
   end,
