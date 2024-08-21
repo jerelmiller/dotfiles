2a61cf9d974ca4001baf00aa55e8f982c543ebee
Jerel Miller <jerelmiller@gmail.com>
2024-08-20 23:22:33 -0600

    Use prettier to format graphql files

--- nvim/.config/nvim/lua/jerel/plugins/formatting.lua
+++ nvim/.config/nvim/lua/jerel/plugins/formatting.lua
@@ -5,6 +5,7 @@ return {
       formatters_by_ft = {
         css = { "prettierd", "prettier" },
         elixir = { "mix" },
+        graphql = { "prettierd", "prettier" },
         lua = { "stylua" },
         javascript = { "prettierd", "prettier" },
         javascriptreact = { "prettierd", "prettier" },
