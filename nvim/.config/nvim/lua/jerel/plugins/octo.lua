92bab4049e6b96b2ff200b6445fe623346260c26
Jerel Miller <jerelmiller@gmail.com>
2024-09-17 23:01:07 -0600

    Update config for octo to use projects v2

--- nvim/.config/nvim/lua/jerel/plugins/octo.lua
+++ nvim/.config/nvim/lua/jerel/plugins/octo.lua
@@ -7,9 +7,7 @@ return {
   },
   config = function()
     require("octo").setup({
-      suppress_missing_scope = {
-        projects_v2 = true,
-      },
+      default_to_projects_v2 = true,
     })
   end,
 }
