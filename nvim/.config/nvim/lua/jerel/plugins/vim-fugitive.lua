fae452dda5a802da53732da74370f5ae1d9ffb3d
Jerel Miller <jerelmiller@gmail.com>
2025-07-01 21:08:56 -0600

    Remove fugitive file

--- nvim/.config/nvim/lua/jerel/plugins/vim-fugitive.lua
+++
@@ -1,9 +0,0 @@
--- return {
---   "tpope/vim-fugitive",
---   config = function()
---     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
---     vim.keymap.set("n", "<leader>gp", function()
---       vim.cmd("Git push")
---     end)
---   end,
--- }
