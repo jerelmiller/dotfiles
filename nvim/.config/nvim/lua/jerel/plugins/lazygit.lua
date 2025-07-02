bcce02d2e52d4c15581520e0638be3106b417a1b
Jerel Miller <jerelmiller@gmail.com>
2025-07-01 20:57:47 -0600

    Add lazygit plugin

---
+++ nvim/.config/nvim/lua/jerel/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+}
