cd6547b25468d576b113a179ff83498210bbfb1c
Jerel Miller <jerelmiller@gmail.com>
2025-07-23 17:33:01 -0600

    Revert "Disable fugitive for now and replace command"
    This reverts commit d1e9fa57dabdae6eb74a44140c76f4ea852bf746.

--- nvim/.config/nvim/lua/jerel/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/jerel/plugins/lazygit.lua
@@ -12,6 +12,6 @@ return {
     "nvim-lua/plenary.nvim",
   },
   keys = {
-    { "<leader>gs", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
 }
