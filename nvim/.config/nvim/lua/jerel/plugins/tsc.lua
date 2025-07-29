08f22f5a078f218e9afacdfa423449fb2deac6ea
Jerel Miller <jerelmiller@gmail.com>
2025-07-29 11:44:28 -0600

    Use trouble from tsc

--- nvim/.config/nvim/lua/jerel/plugins/tsc.lua
+++ nvim/.config/nvim/lua/jerel/plugins/tsc.lua
@@ -1,7 +1,9 @@
 return {
   "dmmulroy/tsc.nvim",
   config = function()
-    require("tsc").setup()
+    require("tsc").setup({
+      use_trouble_qflist = true,
+    })
 
     vim.keymap.set("n", "<leader>ts", vim.cmd.TSC)
   end,
