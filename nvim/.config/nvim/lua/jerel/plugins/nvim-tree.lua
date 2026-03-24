02e7413658fb7573ea1c6751ba1f906c0a1c5a43
Jerel Miller <jerelmiller@gmail.com>
2026-03-24 15:15:39 -0600

    Better file rename

--- nvim/.config/nvim/lua/jerel/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/jerel/plugins/nvim-tree.lua
@@ -8,5 +8,21 @@ return {
       api.tree.toggle({ find_file = true, focus = true })
     end)
     vim.keymap.set("n", "<leader>nt", vim.cmd.NvimTreeToggle)
+
+    local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "NvimTreeSetup",
+      callback = function()
+        local events = require("nvim-tree.api").events
+        events.subscribe(events.Event.NodeRenamed, function(data)
+          if
+            prev.new_name ~= data.new_name or prev.old_name ~= data.old_name
+          then
+            data = data
+            Snacks.rename.on_rename_file(data.old_name, data.new_name)
+          end
+        end)
+      end,
+    })
   end,
 }
