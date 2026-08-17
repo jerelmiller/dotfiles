a34d1362d2052a457242b9fa6eb28259a9f3f734
Jerel Miller <jerelmiller@gmail.com>
2026-08-17 17:48:56 -0600

    Add back mapleader in opts.lua

--- nvim/.config/nvim/lua/jerel/opts.lua
+++ nvim/.config/nvim/lua/jerel/opts.lua
@@ -3,6 +3,9 @@
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
 -- Disable unused providers
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
