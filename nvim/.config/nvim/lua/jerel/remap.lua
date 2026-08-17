17b5f90ef8146f51b7febbcf95ade0d0ec9807bd
Jerel Miller <jerelmiller@gmail.com>
2026-08-16 21:58:34 -0600

    Remove duplicate remap

--- nvim/.config/nvim/lua/jerel/remap.lua
+++ nvim/.config/nvim/lua/jerel/remap.lua
@@ -14,9 +14,6 @@ vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Show code actions
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-
 -- Keep cursor in position when using J
 vim.keymap.set("n", "J", "mzJ`z")
 
