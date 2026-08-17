f5b7b2034ec86ca26c5b197b80235468078fac51
Jerel Miller <jerelmiller@gmail.com>
2026-08-16 20:55:28 -0600

    Fix double setup of mini.comment

--- nvim/.config/nvim/lua/jerel/plugins/mini-comment.lua
+++ nvim/.config/nvim/lua/jerel/plugins/mini-comment.lua
@@ -1,8 +1,13 @@
 return {
   "echasnovski/mini.comment",
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
   opts = {
     options = {
       ignore_blank_line = true,
+      custom_commentstring = function()
+        return require("ts_context_commentstring").calculate_commentstring()
+          or vim.bo.commentstring
+      end,
     },
   },
 }
