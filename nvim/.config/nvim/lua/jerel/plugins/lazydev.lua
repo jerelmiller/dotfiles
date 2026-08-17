f889c6e92e0e9211654c31a2eb1132af6838aacd
Jerel Miller <jerelmiller@gmail.com>
2026-08-16 23:11:27 -0600

    Load luvit types

--- nvim/.config/nvim/lua/jerel/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/jerel/plugins/lazydev.lua
@@ -4,11 +4,11 @@ return {
     ft = "lua",
     opts = {
       library = {
-        "luvit-meta/library",
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
       },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true },
   {
     "saghen/blink.cmp",
     opts = {
