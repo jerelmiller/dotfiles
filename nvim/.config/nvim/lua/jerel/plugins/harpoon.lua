a2ebd4afcb40885cad441a8ea61f5b105e99adb8
Jerel Miller <jerelmiller@gmail.com>
2026-08-16 23:56:11 -0600

    Add extensions

--- nvim/.config/nvim/lua/jerel/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/jerel/plugins/harpoon.lua
@@ -12,6 +12,24 @@ return {
       },
     })
 
+    local ext = require("harpoon.extensions")
+    harpoon:extend(ext.builtins.highlight_current_file())
+    harpoon:extend(ext.builtins.navigate_with_number())
+
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function()
+          harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
+        vim.keymap.set("n", "<C-x>", function()
+          harpoon.ui:select_menu_item({ split = true })
+        end, { buffer = cx.bufnr })
+        vim.keymap.set("n", "<C-t>", function()
+          harpoon.ui:select_menu_item({ tabedit = true })
+        end, { buffer = cx.bufnr })
+      end,
+    })
+
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
     end, { desc = "Harpoon add file" })
