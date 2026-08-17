8b73094e2d9553c735f4199c9cfe166851b345fb
Jerel Miller <jerelmiller@gmail.com>
2026-08-16 22:03:20 -0600

    Remove FixCursorHold

--- nvim/.config/nvim/lua/jerel/plugins/neotest.lua
+++ nvim/.config/nvim/lua/jerel/plugins/neotest.lua
@@ -3,7 +3,6 @@ return {
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "nvim-neotest/neotest-jest",
   },
